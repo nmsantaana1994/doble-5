4ea8805048f13c524f36d711ae881387bd7b1788
nmsantaana1994 <nmsantaana1994@gmail.com>
2023-06-13 20:34:24 -0300

    Carga de partidos

--- src/App.vue
+++ src/App.vue
@@ -26,56 +26,60 @@
     </main>
 
     <template v-if="user.id !== null">
-    <nav class="navbar fixed-bottom bg-light">
-            <div class="container-fluid mx-3">
-                <div class="row justify-content-between align-items-center">
-                    <a class="navbar-toggler col-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </a>
-    
-                    <router-link :to="'/chat'" class="col-2">
-                        <img src="./assets/img/sobre.png" alt="Sobre para mandar mensajes" class="icono-nav" />
-                    </router-link>
-                    
-                    <div class="col-2 fondo-plus rounded">
-                        <router-link :to="`/carga-partidos`">
-                            <img src="./assets/img/plus.png" alt="Icono de agregar partido" class="icono-nav-plus" />
+    <nav class="navbar fixed-bottom bg-dark">
+            <div class="container-fluid align-items-center">
+                <ul class="row m-0 justify-content-center">
+                    <li class="col-2 p-0 mx-1">
+                        <a class="col-2 w-auto" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                            <img src="./assets/img/menu-hamburguesa.png" alt="Sobre para mandar mensajes" class="icono-nav" />
+                        </a>
+                    </li>
+                    <li class="col-2  p-0 mx-1">
+                        <router-link :to="'/chat'">
+                            <img src="./assets/img/sobre.png" alt="Sobre para mandar mensajes" class="icono-nav" />
                         </router-link>
+                    </li>
+                    <li class="col-2  p-0 mx-1">
+                        <router-link :to="`/carga-partidos`"  class="fondo-plus">
+                            <img src="./assets/img/plus.png" alt="Icono de agregar partido" class="icono-nav" />
+                        </router-link>
+                    </li>
+                    <li class="col-2  p-0 mx-1">
+                        <a><img src="./assets/img/campanas.png" alt="Campanas de notificaciones" class="icono-nav" /></a>
+                    </li>
+                    <li class="col-2  p-0 mx-1">
+                        <router-link :to="'/perfil'">
+                            <img src="./assets/img/usuario.png" alt="Icono de usuario" class="icono-nav" />
+                        </router-link>
+                    </li>
+                </ul>
+
+                <!-- Offcanvas -->
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
-    
-                    <a class="col-2"><img src="./assets/img/campanas.png" alt="Campanas de notificaciones" class="icono-nav" /></a>
-    
-                    <router-link :to="'/perfil'" class="col-2">
-                        <img src="./assets/img/usuario.png" alt="Icono de usuario" class="icono-nav" />
-                    </router-link>
-    
-                    <!-- Offcanvas -->
-                    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                        <div class="offcanvas-header">
-                            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                        </div>
-                        <div class="offcanvas-body">
-                            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                <li>
-                                    <div class="d-flex justify-content-between">
-                                        <router-link :to="'/home'" class="col-2 text-decoration-none text-black">
-                                            <img src="./assets/img/hogar.png" alt="Icono de home" class="icono-nav" />
-                                            <p>Home</p>
-                                        </router-link>
-                                    </div>
-                                </li>
-                                <li>
-                                    <form
-                                        action="#"
-                                        method="post"
-                                        @submit.prevent="handleLogout"
-                                    >
-                                        <button type="submit">{{ user.email }} (Cerrar Sesión)</button>
-                                    </form>
-                                </li>
-                            </ul>
-                        </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li>
+                                <div class="d-flex justify-content-between">
+                                    <router-link :to="'/home'" class="col-2 text-decoration-none text-black">
+                                        <img src="./assets/img/hogar.png" alt="Icono de home" class="icono-nav" />
+                                        <p>Home</p>
+                                    </router-link>
+                                </div>
+                            </li>
+                            <li>
+                                <form
+                                    action="#"
+                                    method="post"
+                                    @submit.prevent="handleLogout"
+                                >
+                                    <button type="submit">{{ user.email }} (Cerrar Sesión)</button>
+                                </form>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -112,12 +116,12 @@
 </template>
 
 <style>
+    ul {
+        list-style: none;
+    }
+    
     .icono-nav {
         width: 75%;
-    }
-
-    .icono-nav-plus {
-        width: 100%;
     }
 
     .fondo-plus {
