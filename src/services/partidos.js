a2f49afd05c71427fe54ecdc364bfc0c6d9773e1
nicosartori05 <nicolas.sartori@davinci.edu.ar>
2023-06-13 23:25:49 -0300

    modificaciones para cargar partidos a db

--- src/services/partidos.js
+++ src/services/partidos.js
@@ -1,24 +1,16 @@
 import { doc, collection, serverTimestamp, setDoc, addDoc } from "@firebase/firestore";
 import { db } from "./firebase";
 
-export async function cargarPartido(data/*{nombre, complejo, fecha, hora, cantidadJ, cambios, tipo, valorCancha}*/) {
+export async function cargarPartido(data) {
     const partidoRef = collection(db, "partidos");
 
-    return addDoc(partidoRef, {
-        ...data,
-        // nombre,
-        // complejo,
-        // fecha,
-        // hora,
-        // cantidadJ,
-        // cambios,
-        // tipo,
-        // valorCancha,
-        created_at: serverTimestamp(),
-    })  
-        .then(()=>null)
-        .catch(err => {
-            console.error("[partidos.js cargaPartido] Ocurrió un error al cargar el partido.", err);
-            throw err;
+    try {
+        await addDoc(partidoRef, {
+          ...data,
+          created_at: serverTimestamp(),
         });
+      } catch (err) {
+        console.error("[partidos.js cargaPartido] Ocurrió un error al cargar el partido.", err);
+        throw err;
+      }
 }
