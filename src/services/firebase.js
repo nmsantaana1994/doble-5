064f7741978f46d6883e5612add81ff406e82312
nmsantaana1994 <nmsantaana1994@gmail.com>
2023-06-17 17:37:42 -0300

    Navbar y algunas pruebas

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,6 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAl5gyZFJjjtUe8Bp-DMT3d94fEaOVVIVs",
+//     authDomain: "doble-5-test.firebaseapp.com",
+//     projectId: "doble-5-test",
+//     storageBucket: "doble-5-test.appspot.com",
+//     messagingSenderId: "581763837770",
+//     appId: "1:581763837770:web:af5606b0c26f6a62681bb3"
+//   };
+
 const firebaseConfig = {
     apiKey: "AIzaSyAvD6DEBhAsCF44RImexoURrBB3jkk6eNA",
     authDomain: "clase1-cwm-santa-ana.firebaseapp.com",
