47b56dbcd6f9746d67d59c4d0d76d84707569f7b
nicosartori05 <nicolas.sartori@davinci.edu.ar>
2023-07-07 14:27:13 -0300

    correccion en ver mas en partidos

--- src/composition/usePartidos.js
+++ src/composition/usePartidos.js
@@ -3,14 +3,14 @@ import { getPartidoByNombre } from "../services/partidos.js";
 
 export function usePartido(nombre) {
     const partido = ref({
-            nombre: null,
-            complejo: null,
-            fecha: null,
-            hora: null,
-            cantidadJ: null,
-            cambios: null,
-            tipo: null,
-            valorCancha: null,
+        nombre: null,
+        complejo: null,
+        fecha: null,
+        hora: null,
+        cantidadJ: null,
+        cambios: null,
+        tipo: null,
+        valorCancha: null,
     });
 
     const loading = ref(true);
@@ -18,10 +18,22 @@ export function usePartido(nombre) {
     loadPartido();
 
     async function loadPartido() {
-        const partidoData = await getPartidoByNombre(nombre);
-        console.log(partidoData)
-        partido.value = partidoData;
-        loading.value = false;
+        try {
+            const partidoData = await getPartidoByNombre(nombre);
+            console.log('partido con filtro', partidoData)
+            partido.value.nombre = partidoData.nombre;
+            partido.value.complejo = partidoData.complejo;
+            partido.value.fecha = partidoData.fecha;
+            partido.value.hora = partidoData.hora;
+            partido.value.cantidadJ = partidoData.cantidadJ;
+            partido.value.cambios = partidoData.cambios;
+            partido.value.tipo = partidoData.tipo;
+            partido.value.valorCancha = partidoData.valorCancha;
+            loading.value = false;
+        } catch (error) {
+            console.error(error);
+            loading.value = false;
+        }
     }
 
     return {
