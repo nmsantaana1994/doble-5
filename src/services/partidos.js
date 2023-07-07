38b7ae4f3fb0b6be01f218ca56103aa42edade66
unknown <nmsantaana1994@gmail.com>
2023-07-07 14:53:21 -0300

    Agregando una linea

--- src/services/partidos.js
+++ src/services/partidos.js
@@ -38,6 +38,7 @@ export async function getPartidos() {
 
 export async function getPartidoByNombre(nombre) {
     const q = query(collection(db, "partidos"), where("nombre", "==", nombre));
+    const snapshot = await getDocs(q);
 
   if (snapshot.empty) {
       throw new Error("No existe el partido con el nombre proporcionado");
