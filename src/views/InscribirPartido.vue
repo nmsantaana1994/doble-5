cf86b539b0226843bc6ae4722f003b120dc85eb1
nicosartori05 <nicolas.sartori@davinci.edu.ar>
2023-10-31 14:39:22 -0300

    corrigiendo funcionamiento inscripcion a partidos

--- src/views/InscribirPartido.vue
+++ src/views/InscribirPartido.vue
@@ -6,29 +6,38 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ref, onMounted } from "vue";
 import { getFirestore, updateDoc, doc } from "firebase/firestore";
 import { getPartidoByNombre } from "../services/partidos";
-import { collection, query, where, getDocs } from "firebase/firestore";
-
+import { collection, query, where, getDocs, onSnapshot } from "firebase/firestore";
 
 const auth = getAuth();
 const db = getFirestore();
-const contadorInscriptos = ref([]);
 
 const route = useRoute();
 const { partido, loading } = usePartido(route.params.nombre);
 
 function InscriptionGame() {
   partido.totalJ = partido.cantidadJ * 2;
-  console.log("total de jugadores: ", partido.totalJ);
-  console.log("Inscribiéndome al partido...");
   const user = auth.currentUser;
   if (user) {
-    const uidName = user.displayName;
-  console.log("user",user)
-    contadorInscriptos.value.push(uidName); 
-    actualizarContadorInscriptosPorNombre(partido.value.nombre,contadorInscriptos.value);
+    const userLog = user.displayName;
+    console.log("usuario logeado: ", userLog);
+    console.log(
+      "usuarios inscriptos en partido:",
+      partido.value.contadorInscriptos
+    );
+    let contadorInscriptosPartido = partido.value.contadorInscriptos;
+    console.log(contadorInscriptosPartido.length);
+    if (partido.value.contadorInscriptos.includes(userLog)) {
+      console.log("El usuario ya está inscrito en este partido.");
+      return; // Salir de la función si ya está inscrito
+    } else {
+      contadorInscriptosPartido.push(userLog);
+    }
+    console.log("los equipos se encuentran completos");
+    actualizarListaInscriptos(partido.value.nombre, contadorInscriptosPartido);
   }
 }
-async function actualizarContadorInscriptosPorNombre(nombre, nuevoContador) {
+
+async function actualizarListaInscriptos(nombre, nuevaLista) {
   try {
     const partidosRef = collection(db, "partidos");
     const q = query(partidosRef, where("nombre", "==", nombre));
@@ -36,54 +45,45 @@ async function actualizarContadorInscriptosPorNombre(nombre, nuevoContador) {
 
     querySnapshot.forEach(async (partido) => {
       const partidoRef = doc(db, "partidos", partido.id);
-
       await updateDoc(partidoRef, {
-        contadorInscriptos: nuevoContador,
+        contadorInscriptos: nuevaLista,
       });
-
-      console.log("Contador de inscriptos actualizado correctamente");
+      console.log("lista actualizada correctamente");
+      console.log(
+        "usuarios inscriptos en partido:",
+        partido.value
+      );
     });
   } catch (error) {
     console.error("Error al actualizar contador de inscriptos:", error);
   }
 }
-
-
-
 </script>
 
 <template>
   <LoadingContext :loading="loading">
-    <section class="row py-3 m-0">
-            <div class="col-3 d-flex justify-content-center align-items-center">
-            <router-link :to="`/home`">
-                <img src="../assets/img/flecha-izquierda.png" />
-                <i class="fi fi-sr-angle-left"></i>
-            </router-link>
-            </div>
-            <div class="col-9 d-flex align-items-center">
-                <h1 class="text-center">En construcción <!--Información--></h1>
-            </div>
-    </section>
-    <!-- <section class="row p-1 m-0">
+    <section class="row p-1 m-0">
       <div class="col-12">
         <h2>Inscripcion a partido</h2>
         <h3 class="mb-3 fs-4 text-center">{{ partido.nombre }}</h3>
         <p>{{ partido.fecha }}</p>
         <p>{{ partido.totalJ }}</p>
         <ul>
-          <li v-for="nombreJugador of contadorInscriptos" :key="nombreJugador">
+          <li
+            v-for="nombreJugador of partido.contadorInscriptos"
+            :key="nombreJugador"
+          >
             {{ nombreJugador }}
           </li>
         </ul>
         <button @click="InscriptionGame">Sumarme al partido</button>
       </div>
-    </section> -->
+    </section>
   </LoadingContext>
 </template>
 
 <style scoped>
-  img {
-    width: 80%;
-  }
+img {
+  width: 80%;
+}
 </style>
