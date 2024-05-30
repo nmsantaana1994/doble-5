3b17b834e57dd51a638a6b60c9aa7b5162f02c89
nicosartori05 <nicolassartori@davinci.edu.ar>
2024-05-30 18:23:01 -0300

    correccion para que el chat muestre solo el ultimo mensaje en la previsualizacion

--- src/chat/pages/PrivateChat.vue
+++ src/chat/pages/PrivateChat.vue
@@ -1,114 +1,112 @@
 <script setup>
 import { useRoute } from "vue-router";
 import { useUser } from "../../composition/useUser.js";
-import { dateToString } from "../../helpers/date.js";
 import Button from "../../components/Button.vue";
 import Label from "../../components/Label.vue";
 import Textarea from "../../components/Textarea.vue";
 import { useAuth } from "../../composition/useAuth.js";
-import {onUnmounted, ref, watch} from "vue";
-import {sendPrivateMessage, subscribeToPrivateChat} from "../services/private-chats.js";
+import { onUnmounted, ref, watch } from "vue";
+import { sendPrivateMessage, subscribeToPrivateChat } from "../services/private-chats.js";
 import HeaderChat from "../components/HeaderChat.vue";
 import Section from "../../components/Section.vue";
 import Loading from "../../components/Loading.vue";
 import cardMessage from "../components/cardMessage.vue";
 const route = useRoute();
-const {user: otherUser, loading} = useUser(route.params.id);
-const {user: authUser} = useAuth();
-const {handleSubmit, fields, formLoading} = usePrivateChatForm(authUser, otherUser);
-const {messages, loading: loadingMessages} = usePrivateChat(authUser, otherUser);
+const { user: otherUser, loading } = useUser(route.params.id);
+const { user: authUser } = useAuth();
+const { handleSubmit, fields, formLoading } = usePrivateChatForm(authUser, otherUser);
+const { messages, loading: loadingMessages } = usePrivateChat(authUser, otherUser);
 
 function usePrivateChat(authUser, otherUser) {
-    const loading = ref(true);
-    const messages = ref([]);
-    
-    let unsubscribe = () => {};
+  const loading = ref(true);
+  const messages = ref([]);
 
-   
-    watch(otherUser, newOtherUser => {
-        if(newOtherUser.id != null) {
-            setSubscription();
-        }
-    });
+  let unsubscribe = () => { };
 
-    async function setSubscription() {
-        unsubscribe = await subscribeToPrivateChat(
-            authUser.value.id,
-            otherUser.value.id,
-            newMessages => {
-                messages.value = newMessages;
-                loading.value = false;
-            }
-        );
+
+  watch(otherUser, newOtherUser => {
+    if (newOtherUser.id != null) {
+      setSubscription();
     }
+  });
 
-    onUnmounted(() => {
-        unsubscribe();
-    });
+  async function setSubscription() {
+    unsubscribe = await subscribeToPrivateChat(
+      authUser.value.id,
+      otherUser.value.id,
+      newMessages => {
+        messages.value = newMessages;
+        loading.value = false;
+      }
+    );
+  }
 
-    return {
-        loading,
-        messages,
-    }
+  onUnmounted(() => {
+    unsubscribe();
+  });
+
+  return {
+    loading,
+    messages,
+  }
 }
 
 function usePrivateChatForm(authUser, otherUser) {
-    const formLoading = ref(false);
-    const fields = ref({
-        message: "",
-    });
+  const formLoading = ref(false);
+  const fields = ref({
+    message: "",
+  });
 
-    async function handleSubmit() {
-        formLoading.value = true;
+  async function handleSubmit() {
+    formLoading.value = true;
 
-        try {
-            await sendPrivateMessage(authUser.value.id, otherUser.value.id, fields.value.message);
-            formLoading.value = false;
-            fields.value.message = "";
-        } catch (err) {
-            // TODO: Mostrar un mensaje de feedback de error.
-            console.error("[PrivateChat]", err);
-        }
+    try {
+      await sendPrivateMessage(authUser.value.id, otherUser.value.id, fields.value.message);
+      formLoading.value = false;
+      fields.value.message = "";
+    } catch (err) {
+      // TODO: Mostrar un mensaje de feedback de error.
+      console.error("[PrivateChat]", err);
     }
-    
-    return {
-        formLoading,
-        fields,
-        handleSubmit,
-    }
+  }
+
+  return {
+    formLoading,
+    fields,
+    handleSubmit,
+  }
 }
 
-</script> 
+</script>
 
 <template>
-    <HeaderChat :otherUser="otherUser" />
-    <Section>
-      <Loading :loading="loading" />
-  
-      <div class="mb-3">
-          <ul>
-            <li v-for="message in messages" :key="message.id" class="mb-3" :class="{
-                          'text-end': message.userId == authUser.id}">
-              <cardMessage
-                :message="message"
-                :userName="message.userId === authUser.id ? authUser.displayName || authUser.nombre : otherUser.displayName || otherUser.nombre"
-                :messageClass="message.userId === authUser.id ? 'message-sent' : 'message-received'"
-              />
-            </li>
-          </ul>
-      </div>
-      <div class="mb-3">
-        <form action="#" method="POST" id="form-message" @submit.prevent="handleSubmit">
-          <div class="mb-3">
-            <Label for="message">Mensaje</Label>
-            <Textarea id="message" v-model="fields.message"></Textarea>
-          </div>
-          <Button class="btn btn-primary w-100" :disabled="!fields.message" :class="!fields.message ? 'button_disabled' : ''"/>
-        </form>
-        <div v-if="formLoading" class="mb-3"> Enviando mensaje...</div>
-      </div>
-    </Section>
-  </template>
+  <HeaderChat :otherUser="otherUser" />
+  <Section>
+    <Loading :loading="loading" />
+
+    <div class="mb-3">
+      <ul>
+        <li v-for="message in messages" :key="message.id" class="mb-3" :class="{
+          'text-end': message.userId == authUser.id
+        }">
+          <cardMessage :message="message"
+            :userName="message.userId === authUser.id ? authUser.displayName || authUser.nombre : otherUser.displayName || otherUser.nombre"
+            :messageClass="message.userId === authUser.id ? 'message-sent' : 'message-received'" />
+        </li>
+      </ul>
+    </div>
+    <div>
+      <form action="#" method="POST" id="form-message" @submit.prevent="handleSubmit">
+        <div class="mb-3">
+          <Label for="message">Mensaje</Label>
+          <Textarea id="message" v-model="fields.message"></Textarea>
+        </div>
+        <Button class="w-100" :disabled="!fields.message" :class="!fields.message ? 'button_disabled' : ''" />
+      </form>
+      <div v-if="formLoading" class="mb-3"> Enviando mensaje...</div>
+    </div>
+  </Section>
+</template>
 
 <style scoped>
 ul {
@@ -124,13 +122,17 @@ ul {
   position: fixed;
   width: 100%;
 }
-textarea{
+
+textarea {
   width: 100%;
 }
-form button{
+
+form button {
   background-color: var(--primary-color);
+  color: white;
 }
-.button_disabled{
+
+.button_disabled {
   background-color: grey;
 }
-</style> 
+</style>
