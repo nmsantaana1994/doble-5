04480988b23ba938b872859ffd14f51faaaf66ff
nicosartori05 <nico.sartori.25@gmail.com>
2025-06-26 08:54:53 -0300

    pasar app a pwa

--- src/main.js
+++ src/main.js
@@ -6,6 +6,20 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const app = createApp(App);
 
+
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // Podés mostrar un modal de "Hay una nueva versión"
+    updateSW(true); // O forzarlo
+  },
+  onOfflineReady() {
+    console.log('App lista para funcionar offline');
+  },
+});
+
+
 app.use(router);
 
 app.mount("#app");
